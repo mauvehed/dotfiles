d4c830e1b0c9c636a1d9fc79efcbdaf8cc509f50
mauvehed <nate@mvh.dev>
2025-05-21 10:38:34 -0500

    add op plugins.sh

---
+++ private_dot_config/private_op/plugins.sh
@@ -0,0 +1,7 @@
+export OP_PLUGIN_ALIASES_SOURCED=1
+alias gh="op plugin run -- gh"
+alias doctl="op plugin run -- doctl"
+alias snyk="op plugin run -- snyk"
+alias openai="op plugin run -- openai"
+alias wrangler="op plugin run -- wrangler"
+alias aws="op plugin run -- aws"
